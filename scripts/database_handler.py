a48aed0c92185230f1eb788339fa674981baac7c
Giovy2001 <gchianese01@virgilio.it>
2026-03-14 17:53:28 +0100

    Reverted sync fix

--- scripts/database_handler.py
+++ scripts/database_handler.py
@@ -6,18 +6,15 @@ token = os.getenv("TURSO_AUTH_TOKEN")
 name = "palaenrosadira-routes-sql"
 
 conn = libsql.connect(database=name, sync_url=url, auth_token=token)
-conn.sync()
 
 # Qui aggiungo il nome della via e il grado
 conn.execute("""CREATE TABLE IF NOT EXISTS routes(id INTEGER PRIMARY KEY AUTOINCREMENT, image TEXT, holds TEXT)""")
 conn.commit()
 
 def get_routes():
-    conn.sync()
     return conn.execute("SELECT * FROM routes").fetchall()
 
 def get_route(id):
-    conn.sync()
     return conn.execute("SELECT * FROM routes WHERE id=?", (id,)).fetchone()
 
 def add_route(image_url, holds):
