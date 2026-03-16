be7d62e56192d490a6424e7884efa88e920007e0
Giovy2001 <gchianese01@virgilio.it>
2026-03-16 21:30:55 +0100

    Potential fix for session not lasting. Stopped using cookies for passing variables between pages.

--- pages/create.py
+++ pages/create.py
@@ -9,31 +9,35 @@ def select_image():
 
         image_url = image_handler.put(image.filename, image.read())
 
-        session['last_uploaded_image_url'] = image_url
+        #session['last_uploaded_image_url'] = image_url
 
-        return redirect(url_for("create_route"))
+        return redirect(url_for("create_route", image_url=image_url))
 
     return render_template("select_image.html")
 
 
 def create_route():
-    image_url = session.get('last_uploaded_image_url', None)
-    if not image_url:
-        return redirect(url_for("index"))
+    #image_url = session.get('last_uploaded_image_url', None)
+    image_url = request.args.get("image_url")
+    #if not image_url:
+    #    return redirect(url_for("index"))
     
     if request.method == "POST":
-        session['holds'] = request.form.get("jsonHolds")
-        session['last_uploaded_image_url'] = image_url
+        #session['holds'] = request.form.get("jsonHolds")
+        #session['last_uploaded_image_url'] = image_url
+        holds = request.form.get("jsonHolds")
         
-        return redirect(url_for("save_route")) 
+        return redirect(url_for("save_route", image_url=image_url, holds=holds)) 
 
     return render_template("create_route.html", data={"image_url":image_url})
 
 
 def save_route():    
     if request.method == "POST":
-        image_url = session.get('last_uploaded_image_url', None)
-        holds = session.get('holds', None)
+        #image_url = session.get('last_uploaded_image_url', None)
+        #holds = session.get('holds', None)
+        image_url = request.args.get("image_url")
+        holds = request.args.get("holds")
         
         author = request.form.get("routeAuthor")
         name = request.form.get("routeName")
