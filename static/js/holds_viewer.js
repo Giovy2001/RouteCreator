c4e5874abc9683658bc745442558c87f50840aa3
Giovy2001 <gchianese01@virgilio.it>
2026-03-19 19:20:03 +0100

    Fixed holds scaling outline and added hold use visualization and mapping

--- static/js/holds_viewer.js
+++ static/js/holds_viewer.js
@@ -12,20 +12,31 @@ holds.forEach(h => {
 
     let hold = document.createElement("div");
     hold.classList.add("hold");
-    updateHoldStyle(hold, data);
     hold.dataset.id = data.id;
     holdContainer.appendChild(hold);
+    
+    let use_text = (data.use === "hand") ? "M" : (data.use === "foot") ? "P" : "";
 
-    let use = document.createElement("label");
-    use.textContent = data.use;
-    use.classList.add("use");
-    hold.appendChild(use);
+    if (use_text != "") {
+        let use = document.createElement("label");
+        use.textContent = use_text
+        use.classList.add("use");
+        hold.appendChild(use);
+        use.style.left = `${24 * data.scale + 2}px`;
+        use.style.top = `${12 * data.scale}px`;
+    }
+
+    updateHoldStyle(hold, data);
 })
 
 function updateHoldStyle(hold, data) {
     hold.style.left = `${data.x * 100}%`;
     hold.style.top = `${data.y * 100}%`;
-    hold.style.transform = `translate(-50%,-50%) scale(${data.scale})`;
+
+    hold.style.width = `${24 * data.scale}px`;
+    hold.style.height = `${24 * data.scale}px`;
+
+    
 
     hold.classList.remove("start", "middle", "top");
 
