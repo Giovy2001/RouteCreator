eeeada31deab33905c14403ac7f68be1f6e147aa
Giovy2001 <gchianese01@virgilio.it>
2026-03-20 13:30:49 +0100

    In view mode added buttons to edit or delete route if the current username is the same as the route author.

--- notes.py
+++ notes.py
@@ -1,28 +1,36 @@
 """
-TODO:
-    ## Funzionalità ##
-    
-    1) Username
-    [ ] Se l'username combacia puoi eliminare le tue vie
-    [ ] LOW_PRIORITY Se l'username combacia puoi modificare le tue vie
-    
-    2) Utilizzabilità
-    [ ] Far si che una volta selezionata la presa tu possa scrollare ovunque per posizionarla e non solo sopra.
-        Ad ora è tipo impossibile vedere dove si trova la presa
-    
-    [ ] Cerca tra le vie, per autore / nome
+TODO: Update #2 - Route delete and edit
+    [ ] Funzionalità per
+        [ ] Eliminare il blocco
+        [ ] Modificare nome e descrizione del blocco
+        [ ] Modificare prese blocco
 
-    ## Estetica ##
 
-    [ ] Manca l'icona del sito favicon
+TODO: Update #3 - Rework Route creation
+
+    [ ] Sistema css delle tre fasi di route creation
+        [ ] Aggiungi i tre pallini indicatori della fase
+        [ ] Trovare un modo per far stare tutte opzioni durante la fase di create_route (per via dello scroll bloccato)
+    [ ] Far si che una volta selezionata una presa tu possa scrollare in qualsiasi punto dello schermo per spostarla
+        [ ] Valutare differenza tra de-selezione e hold-drag
+
+
+TODO: Update #4 - qol features
     
-    [ ] Sistemare CSS di ogni pagina
-        [ ] Particolare attenzione a come viene visualizzato da telefono
-        [ ] Sistemare grafica delle creazione blocco
-    
-    ## Pulizia ##
+    [ ] Possibilità di filtrare nell'index, per autore / nome
+        [ ] Singola barra di ricerca filtra qualsiasi risultato coerente
+        [ ] Controlla come si fa la ricerca intelligente (lettera sbagliata ma comunque simile)
+    [ ] Manca l'icona del sito (favicon)
+        
+
+TODO: Update #5 - pulizia del codice
+
+    [ ] Controllare durante l'esecuzione se ci sono errori nella console 
     [ ] Mettere ordine nel codice
     [ ] Aggiungere in python le tipologie delle variabili
     [ ] Commentare tutte le funzioni
     [ ] Rimuovere commenti relativi al precedente modo di passare le variabili con i cookies in create.py
+    [ ] Valutare di rinominare i file per seguire una convenzione migliore
+    [ ] Rimuovere da git i file di cache di python
+    [ ] Cambiare nome a salvataggio nel local storage "boulderUsername" 
 """
